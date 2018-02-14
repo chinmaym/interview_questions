d4c8141b06f7c2ff05618981edd7aab1c8ba08a5
chinmaym <chinmay.m92@gmail.com>
2018-02-14 13:53:24 -0800

    started ctci

---
+++ hackerrank/ctci/left_rotation.cpp
@@ -0,0 +1,74 @@
+/*
+A left rotation operation on an array of size shifts each of the array's elements unit to the left. For example, if left rotations are performed on array , then the array would become .
+
+Given an array of integers and a number, , perform left rotations on the array. Then print the updated array as a single line of space-separated integers.
+
+Input Format
+
+The first line contains two space-separated integers denoting the respective values of (the number of integers) and (the number of left rotations you must perform).
+The second line contains space-separated integers describing the respective elements of the array's initial state.
+
+Constraints
+
+Output Format
+
+Print a single line of space-separated integers denoting the final state of the array after performing left rotations.
+
+Sample Input
+
+5 4
+1 2 3 4 5
+
+Sample Output
+
+5 1 2 3 4
+*/
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    for(int i=0;i<k;i++)
+    {
+        int temp = a.front();
+        a.erase(a.begin());
+        a.push_back(temp);
+    }
+    return a;
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
+    return 0;
+}
