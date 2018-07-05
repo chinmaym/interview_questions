85294f305eb5254025687dcb590a8cf654735005
chinmaym <chinmay.m92@gmail.com>
2018-07-05 13:06:09 -0700

    Update 198_house_robber.py

--- leetcode/198_house_robber.py
+++ leetcode/198_house_robber.py
@@ -3,13 +3,12 @@
 
 def main():
 	nums = map(int,raw_input().split())
-	incl = 0; excl = 0
-	for i in nums:
-		new_excl = excl if excl > incl else incl
-		incl = excl + i
-		excl = new_excl
-	return max(excl,incl)
-
+        prev = cur = 0
+        for i in nums:
+            temp = cur
+            cur = max(cur,prev + i)
+            prev = temp
+        return cur
 
 if __name__ == '__main__':
 	print main()
