6d1bd23c92b848144ee2048ae12716d6d91d8eea
chinmaym <chinmay.m92@gmail.com>
2018-07-05 11:51:32 -0700

    Create 70_climbing_stairs.py

---
+++ leetcode/70_climbing_stairs.py
@@ -0,0 +1,36 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+Note: Given n will be a positive integer.
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+'''
+memory = {0:0,
+          1:1,
+          2:2,
+          3:3}
+
+def NumWays(n):
+    # if n<=0:
+    #     return 1
+    if n in memory:
+        return memory[n]
+    memory[n] = NumWays(n-1) + NumWays(n-2)
+    return memory[n]
+
+
+if __name__=="__main__":
+    # n = input()
+    n = 4
+    print NumWays(n)
